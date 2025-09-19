e74f5ab59970506c69bb69750b6ed5274614ab19
STUDENT-PC <crcalderon7360pam@student.fatima.edu.ph>
2025-09-19 10:57:54 +0800

    loyd

--- admin/payroll.php
+++ admin/payroll.php
@@ -1,28 +1,31 @@
 <?php
 session_start();
-if (!isset($_SESSION['username'])) 
-{
-die(header('Location: ../index.php'));
+if (!isset($_SESSION['username'])) {
+    header('Location: ../index.php');
+    exit;
 }
 
+// Include your database connection (make sure $conn is defined there)
+include('connection.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8" />
 <title>Calculate Payroll</title>
 <link rel="stylesheet" href="../css/style.css" type="text/css" />
 <script src="../../SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
 <link href="../../SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body><div id="outerwrapper">
+<body>
+<div id="outerwrapper">
 <div id="header"></div>
 <div id="links">
   <ul id="MenuBar1" class="MenuBarHorizontal">
-    <li><a href="index.php">HOME</a>      </li>
+    <li><a href="index.php">HOME</a></li>
     <li><a href="reg_staff.php">REGISTER STAFF</a></li>
-    <li><a href="view_staff.php">VIEW STAFF</a>      </li>
+    <li><a href="view_staff.php">VIEW STAFF</a></li>
     <li><a href="payroll.php" class="MenuBarItemSubmenu">PAYROLL</a>
       <ul>
         <li><a href="print.php">Print Slip</a></li>
@@ -30,8 +33,8 @@ die(header('Location: ../index.php'));
     </li>
     <li><a href="#" class="MenuBarItemSubmenu">MESSAGE</a>
       <ul>
-<li><a href="inbox.php">Inbox</a></li>
-<li><a href="sentmessages.php">Sent</a></li>
+        <li><a href="inbox.php">Inbox</a></li>
+        <li><a href="sentmessages.php">Sent</a></li>
       </ul>
     </li>
     <li><a href="../logout.php">Logout</a></li>
@@ -39,16 +42,11 @@ die(header('Location: ../index.php'));
 </div>
 <div id="body">
 
+<?php
+// Fetch records with mysqli
+$qry = mysqli_query($conn, "SELECT * FROM register_staff") or die(mysqli_error($conn));
 
-
-
-  <?php
-//database connection
-include('connection.php');
-
-//view record
-$qry = mysql_query("SELECT * FROM register_staff");
-echo "<table border='1' align='center'>
+echo "<table border='1' align='center' cellpadding='5' cellspacing='0'>
 <tr>
 <th>Staff ID</th>
 <th>Full Name</th>
@@ -59,33 +57,36 @@ echo "<table border='1' align='center'>
 <th>Grade</th>
 <th>Years</th>
 <th>Date Employed</th>
-
+<th>Action</th>
 </tr>";
 
-
-while ($row = mysql_fetch_array($qry))
-{
-	echo "<tr>";
-	echo "<td>" .$row['staff_id'] . "</td>";
-	echo "<td>" .$row['fname'] . "</td>";
-	echo "<td>" .$row['sex'] . "</td>";
-	echo "<td>" .$row['birthday'] . "</td>";
-	echo "<td>" .$row['department'] . "</td>";
-	echo "<td>" .$row['position'] . "</td>";
-	echo "<td>" .$row['grade'] . "</td>";
-	echo "<td>" .$row['years'] . "</td>";
-	echo "<td>" .$row['date_registered'] . "</td>";
-	echo "<td> <a href= pay.php?id=".$row['staff_id'].">Payroll</a>";
-	echo "</tr>";
+while ($row = mysqli_fetch_assoc($qry)) {
+    echo "<tr>";
+    echo "<td>" . htmlspecialchars($row['staff_id']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['fname']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['sex']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['birthday']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['department']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['position']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['grade']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['years']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['date_registered']) . "</td>";
+    echo "<td><a href='pay.php?id=" . urlencode($row['staff_id']) . "'>Payroll</a></td>";
+    echo "</tr>";
 }
 echo "</table>";
-echo "<a href=index.php>Go Home</a> <br />";
-echo "<a href=payroll.php>Calculate Payroll</a>";
+
+echo "<p><a href='index.php'>Go Home</a></p>";
+echo "<p><a href='payroll.php'>Calculate Payroll</a></p>";
 ?>
+
 </div>
 </div>
 <script type="text/javascript">
-var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"../../SpryAssets/SpryMenuBarDownHover.gif", imgRight:"../../SpryAssets/SpryMenuBarRightHover.gif"});
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {
+    imgDown:"../../SpryAssets/SpryMenuBarDownHover.gif", 
+    imgRight:"../../SpryAssets/SpryMenuBarRightHover.gif"
+});
 </script>
 </body>
 </html>
--- admin/print.php
+++ admin/print.php
@@ -1,32 +1,31 @@
 <?php
 session_start();
-if (!isset($_SESSION['username'])) 
-{
-die(header('Location: ../index.php'));
+if (!isset($_SESSION['username'])) {
+    header('Location: ../index.php');
+    exit;
 }
 
-?>
-<?php
 //database connection
 include('connection.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8" />
 <title>Print PaySlip</title>
 <link rel="stylesheet" href="../css/style.css" type="text/css" />
 <script src="../../SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
 <link href="../../SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body><div id="outerwrapper">
+<body>
+<div id="outerwrapper">
 <div id="header"></div>
 <div id="links">
   <ul id="MenuBar1" class="MenuBarHorizontal">
-    <li><a href="index.php">HOME</a>      </li>
+    <li><a href="index.php">HOME</a></li>
     <li><a href="reg_staff.php">REGISTER STAFF</a></li>
-    <li><a href="view_staff.php">VIEW STAFF</a>      </li>
+    <li><a href="view_staff.php">VIEW STAFF</a></li>
     <li><a href="payroll.php" class="MenuBarItemSubmenu">PAYROLL</a>
       <ul>
         <li><a href="print.php">Print Slip</a></li>
@@ -34,19 +33,21 @@ include('connection.php');
     </li>
     <li><a href="#" class="MenuBarItemSubmenu">MESSAGE</a>
       <ul>
-<li><a href="inbox.php">Inbox</a></li>
-<li><a href="sentmessages.php">Sent</a></li>
+        <li><a href="inbox.php">Inbox</a></li>
+        <li><a href="sentmessages.php">Sent</a></li>
       </ul>
     </li>
     <li><a href="../logout.php">Logout</a></li>
   </ul>
 </div>
+
 <div id="body">
 
 <?php
-//view record
-$qry = mysql_query("SELECT * FROM salary");
-echo "<table border='1' align='center'>
+// Use mysqli to query salary table
+$qry = mysqli_query($conn, "SELECT * FROM salary") or die(mysqli_error($conn));
+
+echo "<table border='1' align='center' cellpadding='5' cellspacing='0'>
 <tr>
 <th>SID</th>
 <th>ID</th>
@@ -64,39 +65,43 @@ echo "<table border='1' align='center'>
 <th>Tax</th>
 <th>Total</th>
 <th>Date</th>
-
+<th>Delete</th>
+<th>Print</th>
 </tr>";
 
-
-while ($row = mysql_fetch_array($qry))
-{
-	echo "<tr>";
-	echo "<td>" .$row['salary_id'] . "</td>";
-	echo "<td>" .$row['staff_id'] . "</td>";
-	echo "<td>" .$row['fname'] . "</td>";
-	echo "<td>" .$row['department'] . "</td>";
-	echo "<td>" .$row['position'] . "</td>";
-	echo "<td>" .$row['grade'] . "</td>";
-	echo "<td>" .$row['years'] . "</td>";
-	echo "<td>" .round($row['basic']) . "</td>";
-	echo "<td>" .round($row['meal']) . "</td>";
-	echo "<td>" .round($row['housing']) . "</td>";
-	echo "<td>" .round($row['transport']) . "</td>";
-	echo "<td>" .round($row['entertainment']) . "</td>";
-	echo "<td>" .round($row['long_service']) . "</td>";
-	echo "<td>" .round($row['tax']) . "</td>";
-	echo "<td>" .round($row['totall']) . "</td>";
-	echo "<td>" .$row['date_s'] . "</td>";
-	echo "<td> <a href= salary_delete.php?salary_id=".$row['salary_id']."&staff_id=".$row['staff_id']."&salary_id=".$row['salary_id'].">Delete</a>";
-	echo "<td> <a href= payslip.php?staff_id=".$row['staff_id']."&salary_id=".$row['salary_id'].">Print</a>";
-	echo "</tr>";
+while ($row = mysqli_fetch_assoc($qry)) {
+    echo "<tr>";
+    echo "<td>" . htmlspecialchars($row['salary_id']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['staff_id']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['fname']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['department']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['position']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['grade']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['years']) . "</td>";
+    echo "<td>" . round($row['basic']) . "</td>";
+    echo "<td>" . round($row['meal']) . "</td>";
+    echo "<td>" . round($row['housing']) . "</td>";
+    echo "<td>" . round($row['transport']) . "</td>";
+    echo "<td>" . round($row['entertainment']) . "</td>";
+    echo "<td>" . round($row['long_service']) . "</td>";
+    echo "<td>" . round($row['tax']) . "</td>";
+    echo "<td>" . round($row['totall']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['date_s']) . "</td>";
+    echo "<td><a href='salary_delete.php?salary_id=" . urlencode($row['salary_id']) . "&staff_id=" . urlencode($row['staff_id']) . "'>Delete</a></td>";
+    echo "<td><a href='payslip.php?staff_id=" . urlencode($row['staff_id']) . "&salary_id=" . urlencode($row['salary_id']) . "'>Print</a></td>";
+    echo "</tr>";
 }
-echo "</table>"
+echo "</table>";
 ?>
+
 </div>
 </div>
+
 <script type="text/javascript">
-var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"../../SpryAssets/SpryMenuBarDownHover.gif", imgRight:"../../SpryAssets/SpryMenuBarRightHover.gif"});
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {
+    imgDown:"../../SpryAssets/SpryMenuBarDownHover.gif",
+    imgRight:"../../SpryAssets/SpryMenuBarRightHover.gif"
+});
 </script>
 
 </body>
