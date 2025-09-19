aa66dc1bc29f6da17de9edec9fd48b8bc3cfc345
crcalderon00 <crcalderon7360pam@student.fatima.edu.ph>
2025-09-19 12:23:32 +0800

    loydloyd

--- employee/index.php
+++ employee/index.php
@@ -9,18 +9,18 @@ if (!isset($_SESSION['staff_id'])) {
 
 $staff_id = $_SESSION['staff_id'];
 
-// Use mysqli_query instead of mysql_query
+// Fetch staff details
 $qry = mysqli_query($conn, "SELECT * FROM register_staff WHERE staff_id = '$staff_id'");
-
 if (!$qry) {
     die('Query failed: ' . mysqli_error($conn));
 }
+$row = mysqli_fetch_assoc($qry);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Employee Dashboard</title>
 <link rel="stylesheet" href="../css/staff.css" type="text/css" />
 <script src="../../SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
 <link href="../../SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
@@ -28,66 +28,68 @@ if (!$qry) {
 
 <body>
 <div id="outerwrapper">
-<div id="header"><img src="../images/staffhead.jpg" /></div>
-<div id="links">
-  <?php include('link.php'); ?>
-</div>
-<div id="body">
-  <table width="1000" border="1">
-    <tr>
-      <td width="124">&nbsp;</td>
-      <td width="860" rowspan="5" align="left" valign="top">
-        <table width="100%" border="0">
-          <tr>
-            <td width="46%" rowspan="3" align="left" valign="top">
-              <table width="395" border="1" align="center">
-                <?php while ($row = mysqli_fetch_assoc($qry)) { ?>
-                <tr>
-                  <td width="126"><strong>Staff ID</strong></td>
-                  <td width="237">&nbsp;<?php echo htmlspecialchars($row['staff_id']); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Full Name</strong></td>
-                  <td>&nbsp;<?php echo htmlspecialchars($row['fname']); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Department</strong></td>
-                  <td>&nbsp;<?php echo htmlspecialchars($row['department']); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Position</strong></td>
-                  <td>&nbsp;<?php echo htmlspecialchars($row['position']); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Date Joined</strong></td>
-                  <td>&nbsp;<?php echo htmlspecialchars($row['date_registered']); ?></td>
-                </tr>
-                <?php } ?>
-              </table>
-            </td>
-            <td width="48%">&nbsp;</td>
-            <td width="3%">&nbsp;</td>
-            <td width="3%">&nbsp;</td>
-          </tr>
-          <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-          <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td><img src="../images/UlIqmHJn-SK.gif" width="124" height="110" /></td>
-    </tr>
-    <tr>
-      <td><a href="profile.php">View Complete Profile</a></td>
-    </tr>
-    <tr>
-      <td><a href="resetpassword.php" class="bx2" rel="470-200">Change Password</a></td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-    </tr>
-  </table>
-</div>
+  <div id="header"><img src="../images/staffhead.jpg" alt="Staff Header" /></div>
+  <div id="links">
+    <?php include('link.php'); ?>
+  </div>
+  <div id="body">
+    <table width="1000" border="1">
+      <tr>
+        <td width="124" align="center" valign="top">
+          <!-- Profile photo -->
+          <?php if (!empty($row['profile_photo'])) { ?>
+            <img src="../<?php echo htmlspecialchars($row['profile_photo']); ?>" width="124" height="110" style="border-radius:50%;" />
+          <?php } else { ?>
+            <img src="../images/default.png" width="124" height="110" style="border-radius:50%;" />
+          <?php } ?>
+        </td>
+        <td width="860" rowspan="5" align="left" valign="top">
+          <table width="100%" border="0">
+            <tr>
+              <td width="46%" rowspan="3" align="left" valign="top">
+                <table width="395" border="1" align="center">
+                  <tr>
+                    <td width="126"><strong>Staff ID</strong></td>
+                    <td width="237">&nbsp;<?php echo htmlspecialchars($row['staff_id']); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Full Name</strong></td>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['fname']); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Department</strong></td>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['department']); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Position</strong></td>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['position']); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Date Joined</strong></td>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['date_registered']); ?></td>
+                  </tr>
+                </table>
+              </td>
+              <td width="48%">&nbsp;</td>
+              <td width="3%">&nbsp;</td>
+              <td width="3%">&nbsp;</td>
+            </tr>
+            <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+            <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td align="center"><a href="profile.php">View Complete Profile</a></td>
+      </tr>
+      <tr>
+        <td align="center"><a href="resetpassword.php" class="bx2" rel="470-200">Change Password</a></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </div>
 </div>
 
 <script type="text/javascript" src="../css/mootools.js"></script> 
--- employee/profile.php
+++ employee/profile.php
@@ -1,5 +1,5 @@
 <?php
-include('../admin/connection.php'); // make sure $conn is defined here as your mysqli connection
+include('../admin/connection.php');
 session_start();
 if (!isset($_SESSION['staff_id'])) {
     header('Location: ../index.php');
@@ -8,73 +8,68 @@ if (!isset($_SESSION['staff_id'])) {
 
 $staff_id = $_SESSION['staff_id'];
 
-// Use mysqli_query with $conn as first param
-$qry = mysqli_query($conn, "SELECT * FROM register_staff WHERE staff_id = '$staff_id'");
+// Handle photo upload
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['profile_photo'])) {
+    $targetDir = "../uploads/";
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0777, true);
+    }
+    $fileName = basename($_FILES["profile_photo"]["name"]);
+    $targetFile = $targetDir . $staff_id . "_" . $fileName;
 
-// Check for query error
-if (!$qry) {
-    die("Database query failed: " . mysqli_error($conn));
+    if (move_uploaded_file($_FILES["profile_photo"]["tmp_name"], $targetFile)) {
+        $relativePath = "uploads/" . $staff_id . "_" . $fileName;
+        $update = "UPDATE register_staff SET profile_photo = '$relativePath' WHERE staff_id = '$staff_id'";
+        mysqli_query($conn, $update);
+    } else {
+        echo "Error uploading file.";
+    }
 }
+
+// Fetch staff data
+$qry = mysqli_query($conn, "SELECT * FROM register_staff WHERE staff_id = '$staff_id'");
+if (!$qry) {
+    die("Query failed: " . mysqli_error($conn));
+}
+$tbl = mysqli_fetch_assoc($qry);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
 <title>Staff Profile</title>
 <link rel="stylesheet" href="../css/staff.css" type="text/css" />
-<script src="../../SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
-<link href="../../SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
 </head>
-
 <body>
 <div id="outerwrapper">
 <div id="header"><img src="../images/staffhead.jpg" alt="Staff Header" /></div>
-<div id="links">
-  <?php include('link.php'); ?>
-</div>
+<div id="links"><?php include('link.php'); ?></div>
 <div id="body">
-  <table width="410" border="1" align="center" cellpadding="5" cellspacing="3">
-    <?php while ($tbl = mysqli_fetch_assoc($qry)) { ?>
+  <table border="1" align="center" cellpadding="5" cellspacing="3">
     <tr>
-      <td width="120"><strong>Staff ID</strong></td>
-      <td width="271"><?php echo htmlspecialchars($tbl['staff_id']); ?></td>
+      <td colspan="2" align="center">
+        <?php if (!empty($tbl['profile_photo'])) { ?>
+          <img src="../<?php echo htmlspecialchars($tbl['profile_photo']); ?>" width="150" height="150" style="border-radius:50%;" />
+        <?php } else { ?>
+          <img src="../images/default.png" width="150" height="150" style="border-radius:50%;" />
+        <?php } ?>
+        <form method="post" enctype="multipart/form-data">
+          <input type="file" name="profile_photo" required>
+          <button type="submit">Upload Photo</button>
+        </form>
+      </td>
     </tr>
-    <tr>
-      <td><strong>Name</strong></td>
-      <td><?php echo htmlspecialchars($tbl['fname']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Sex</strong></td>
-      <td><?php echo htmlspecialchars($tbl['sex']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Birthday</strong></td>
-      <td><?php echo htmlspecialchars($tbl['birthday']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Position</strong></td>
-      <td><?php echo htmlspecialchars($tbl['position']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Department</strong></td>
-      <td><?php echo htmlspecialchars($tbl['department']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Grade</strong></td>
-      <td><?php echo htmlspecialchars($tbl['grade']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Years Spent</strong></td>
-      <td><?php echo htmlspecialchars($tbl['years']); ?></td>
-    </tr>
-    <tr>
-      <td><strong>Date Registered</strong></td>
-      <td><?php echo htmlspecialchars($tbl['date_registered']); ?></td>
-    </tr>
-    <?php } ?>
+    <tr><td><strong>Staff ID</strong></td><td><?php echo htmlspecialchars($tbl['staff_id']); ?></td></tr>
+    <tr><td><strong>Name</strong></td><td><?php echo htmlspecialchars($tbl['fname']); ?></td></tr>
+    <tr><td><strong>Sex</strong></td><td><?php echo htmlspecialchars($tbl['sex']); ?></td></tr>
+    <tr><td><strong>Birthday</strong></td><td><?php echo htmlspecialchars($tbl['birthday']); ?></td></tr>
+    <tr><td><strong>Position</strong></td><td><?php echo htmlspecialchars($tbl['position']); ?></td></tr>
+    <tr><td><strong>Department</strong></td><td><?php echo htmlspecialchars($tbl['department']); ?></td></tr>
+    <tr><td><strong>Grade</strong></td><td><?php echo htmlspecialchars($tbl['grade']); ?></td></tr>
+    <tr><td><strong>Years Spent</strong></td><td><?php echo htmlspecialchars($tbl['years']); ?></td></tr>
+    <tr><td><strong>Date Registered</strong></td><td><?php echo htmlspecialchars($tbl['date_registered']); ?></td></tr>
   </table>
 </div>
 </div>
-
 </body>
 </html>
